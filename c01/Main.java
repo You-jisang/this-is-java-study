6ff3e7052c4bec8fe5b86012988bd120137b759f
You-jisang <dbwltkd1019@naver.com>
2023-01-04 21:56:23 +0900

    feat: hi

--- c01/Main.java
+++ c01/Main.java
@@ -2,6 +2,7 @@ package c01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello c01!");
+        System.out.println("hello hello!");
+        System.out.println("hi c01!");
     }
 }
